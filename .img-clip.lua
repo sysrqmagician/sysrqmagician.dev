7d854d4e1cbe84ffd9a7c3eca511f1622c1ba436
sysrqmagician <sysrqmagician@proton.me>
2025-10-20 13:45:40 +0200

    chore: update img-clip config

--- .img-clip.lua
+++ .img-clip.lua
@@ -2,7 +2,7 @@ return {
   filetypes = {
     markdown = {
       dir_path = "static/assets",
-      template = "![$CURSOR](/static/$FILE_NAME)"
+      template = "![$CURSOR](/assets/$FILE_NAME)"
     }
   }
 }
